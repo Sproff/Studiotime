b9e67b2ca65092e8c87a08096a541540c82992b5
Sproff <sproff.oluwaseun@gmail.com>
2020-10-06 20:43:57 +0100

    added index

---
+++ js/script.js
@@ -0,0 +1,13 @@
+ /*=================== MOBILE NAVIGATION BAR =====================*/
+$("[data-trigger]").on("click", function(){
+    let trigger_id =  $(this).attr('data-trigger');
+    $(trigger_id).toggleClass("show");
+    $('body').toggleClass("offcanvas-active");
+});
+
+// close button 
+$(".btn-close").click(function(e){
+    $(".navbar-collapse").removeClass("show");
+    $("body").removeClass("offcanvas-active");
+}); 
+
